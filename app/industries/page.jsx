dd89e414790992fca8df81d246c8b6537dc3f69b
Rajesh Naik <rajeshnaik@Rajesh-2.local>
2025-11-24 19:35:12 +0530

    code added

--- app/industries/page.jsx
+++ app/industries/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 
+import IndustriesHero from "./IndustriesHero";
+
 export default function Industries() {
     return (
         <>
-           <h1>Indutries Page</h1>
+           <IndustriesHero/>
         </>
      
     )
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 "use client";
 
 import './globals.css';
-import { Provider } from 'react-redux';
-import { store } from '../store/stores.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import { Lexend } from 'next/font/google'; 
@@ -13,24 +11,18 @@ const lexend = Lexend({
   variable: '--font-lexend', 
 });
 
-
 export default function RootLayout({ children }) {
   return (
-    <Provider store={store}>
-    
     <html lang="en" className={`scroll-smooth ${lexend.variable}`}>
       <body> 
-       
-          {/* Navbar can now access theme context */}
-          <Navbar />
-          
-          <main className="flex-grow pt-20 min-h-screen">
-              {children}
-          </main>
-          
-          <Footer />
+        <Navbar />
+        
+        <main className="flex-grow pt-20 min-h-screen">
+          {children}
+        </main>
+
+        <Footer />
       </body>
     </html>
-    </Provider>
   );
 }
